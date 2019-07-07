1d2c3f24228f171b05e9bf8d26ef2b4785467843
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-06 23:26:42 -0400

    despite my best attempts to over complicate the issue, the code for the timer is finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
-    
-$('.test2').text('jquery linked')
 
-console.log('js linked');
+    //timer
+    //----------------------------------------------------------------------------------------
+    let intervalId;
+    let time = 300;
 
+    run = () => {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 100);
+    }
+
+    decrement = () => {
+        time--;
+        $('.timer').html('<h2>' + time + '</h2>');
+        if (time === 0) {
+            stop();
+            alert('Time Up!');
+        }
+    }
+
+    stop = () => {
+        clearInterval(intervalId);
+    }
+
+    run();
+
+    //-------------------------------------------------------------------------------------------
 
 
 });
